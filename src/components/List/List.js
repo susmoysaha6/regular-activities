d7751556b92c114990446d24c13530c9f00b83ca
susmoysaha6 <susmoysaha6@gmail.com>
2022-09-29 04:49:12 +0600

    personal info added to list container

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import './List.css'
+import './List.css';
+import my from '../../images/my.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+
+
 
 const List = () => {
     return (
-        <div>
-            <h1>This is List</h1>
+        <div className='list-container'>
+
+            <div className='personal-info'>
+                <img src={my} alt="" />
+                <div>
+                    <p className='user-name' >Susmoy</p>
+                    <p className='user-address'> <small><FontAwesomeIcon icon={faLocationDot} ></FontAwesomeIcon>  272, Bazar Road, Barishal </small></p>
+                </div>
+            </div>
+            <div className="info-container">
+                <div>
+                    <h2>75<small>kg Weight</small></h2>
+
+                </div>
+                <div>
+                    <h2>5.7<small>" Height</small></h2>
+                </div>
+                <div>
+                    <h2>27<small>yrs Age</small></h2>
+                </div>
+            </div>
         </div>
+
     );
 };
 
