55ab025705131003617b4d76bead5b3301a10df7
susmoysaha6 <susmoysaha6@gmail.com>
2022-09-29 03:44:55 +0600

    responsiveness implemented

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Activities.css'
+import './Activities.css';
 import Activity from '../Acitivity/Activity';
 
 const Activities = ({ activities }) => {
     return (
         <div className='activities-container'>
-            <h3>Select Regular Activities</h3>
+            <h3 className='activities-heading'>Select Regular Activities</h3>
             <div className='card-continer'>
                 {
                     activities.map(activity => <Activity key={activity.id} activity={activity} ></Activity>)
