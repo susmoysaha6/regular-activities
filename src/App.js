84c9d792da30a3f8a55be9b298d6b80238d5236e
susmoysaha6 <susmoysaha6@gmail.com>
2022-09-29 14:43:48 +0600

    Add a break , activity details and react toastify added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Home from './components/Home/Home';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
       <Home></Home>
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,32 +3,57 @@ import './List.css';
 import my from '../../images/my.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { toast } from 'react-toastify';
 
 
 
 const List = () => {
+    const handleCompleted = () => {
+        toast("Activity Completed For Today");
+    }
     return (
         <div className='list-container'>
-
             <div className='personal-info'>
                 <img src={my} alt="" />
                 <div>
-                    <p className='user-name' >Susmoy</p>
+                    <h4 className='user-name' >Susmoy Saha</h4>
                     <p className='user-address'> <small><FontAwesomeIcon icon={faLocationDot} ></FontAwesomeIcon>  272, Bazar Road, Barishal </small></p>
                 </div>
             </div>
             <div className="info-container">
-                <div>
-                    <h2>75<small>kg Weight</small></h2>
+                <div className='info'>
+                    <h4 >75<small>kg Weight</small></h4>
 
                 </div>
-                <div>
-                    <h2>5.7<small>" Height</small></h2>
+                <div className='info'>
+                    <h4>5.7<small>" Height</small></h4>
                 </div>
-                <div>
-                    <h2>27<small>yrs Age</small></h2>
+                <div className='info'>
+                    <h4>27<small>yrs Age</small></h4>
                 </div>
             </div>
+            <div>
+                <h3>Add A Break</h3>
+                <div className='break-container'>
+                    <button className='break-btn'>15m</button>
+                    <button className='break-btn'>20m</button>
+                    <button className='break-btn'>30m</button>
+                    <button className='break-btn'>45m</button>
+                    <button className='break-btn'>60m</button>
+                </div>
+            </div>
+            <div>
+                <h3>Activity Details</h3>
+                <div className='time-container'>
+                    <h3>Activity Time</h3>
+                    <p>0</p>
+                </div>
+                <div className='time-container'>
+                    <h3>Break Time</h3>
+                    <p>0</p>
+                </div>
+            </div>
+            <button onClick={handleCompleted} className='completed-btn'>Activity Completed</button>
         </div>
 
     );
